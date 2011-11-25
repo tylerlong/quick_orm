87aa834001d68cc7f2ea554c268886ae5e540ac3
Tyler Long <tyler4long@gmail.com>
2011-11-25 07:15:20 -0500

    improved tests.py, automatically install the newest code before testing

--- tests.py
+++ tests.py
@@ -2,9 +2,11 @@ import sys, subprocess
 from toolkit_library.inspector import PackageInspector
 from quick_orm import examples
 
+# automatically install the newest code before testing
+subprocess.call([sys.executable, 'setup.py', 'install'])
 
-for module in PackageInspector(examples).get_all_modules():
+# call each example file and print the result
+for module in PackageInspector(examples).get_all_modules():    
     print '****** {0} ******'.format(module)
-    theproc = subprocess.Popen([sys.executable, "quick_orm/examples/{0}.py".format(module)])
-    theproc.communicate()
+    subprocess.call([sys.executable, 'quick_orm/examples/{0}.py'.format(module)])
     print
